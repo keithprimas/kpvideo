8cee173d133dd8876c3ffe281f8a75e1e474e290
Keith Primas <keithprimas@gmail.com>
2024-05-02 21:29:47 -0400

    working on sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,32 @@
+"use client"
 import React from 'react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { sidebarLinks } from '@/constants';
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <div>Sidebar</div>
+    <section className='sticky left-0 top-0 flex h-screen w-fit 
+    flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+      <div className='flex flex-col gap-6'>
+          { sidebarLinks.map((link) => {
+            const isActive = pathname === link.route || pathname.startsWith(link.route);
+
+            return (
+              <Link 
+              href={link.route}
+              key={link.label}
+              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {'bg-blue-1': isActive})}
+              >
+              {link.label}
+              </Link>
+            )
+          })}
+      </div>
+    </section>
   )
 }
 
