530abde7a52e58ddc3663eaf63c3deb7641ba8d9
Keith Primas <keithprimas@gmail.com>
2024-05-07 22:25:32 -0400

    working on streamvideo api

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,13 +1,10 @@
 "use client"
 import React, { ReactNode } from 'react'
 
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-  } from "@/components/ui/dialog"
+import { Dialog, DialogContent } from "@/components/ui/dialog"
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
 
 interface MeetingModalProps {
     isOpen: boolean;
@@ -23,19 +20,37 @@ interface MeetingModalProps {
 
   
 
-
-
 const MeetingModal = ({ isOpen, onClose, title, className, children, handleClick, buttonText, image, buttonIcon } : MeetingModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-        <DialogContent>
-            <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
-            </DialogDescription>
-            </DialogHeader>
+        <DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-box-1 px-6 py-9 text-white'>
+            <div className='flex flex-col gap-6'>
+                {image && (
+                    <div className='flex justify-center'>
+                      <Image 
+                      src={image}
+                      alt='image'
+                      width={72}
+                      height={72}
+                      />
+                    </div>
+                )}
+                <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>{title}</h1>
+                {children}
+                <Button className='bg-box-4 text-white 
+                focus-visible:ring-0 
+                focus-visible:ring-offset-0' onClick={handleClick}>
+                  {buttonIcon && (
+                    <Image 
+                    src={buttonIcon}
+                    alt='button icon'
+                    width={13}
+                    height={13}
+                    />
+                  )} &nbsp;
+                  {buttonText || 'Schedule Meeting'}
+                </Button>
+            </div>
         </DialogContent>
     </Dialog>
 
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,14 +3,49 @@ import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import MeetingModal from './MeetingModal'
 import { useRouter } from 'next/navigation';
+import { useUser } from '@clerk/nextjs';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = 
-  useState<"isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined>(undefined)
-  
-  const createMeeting = () => {
+  useState<"isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined>(undefined);
 
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({ 
+    dateTime: new Date(),
+    description: '',
+    link: ''
+   })
+   
+   const [callDetails, setCallDetails] = useState<Call>()
+  
+  const createMeeting = async () => {
+    if(!client || !user) return;
+
+    try {
+      const id = crypto.randomUUID();
+      const call = client.call('default', id);
+
+      if(!call) throw new Error('Failed to create call');
+
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || 'Instant meeting';
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description
+          }
+        }
+      })
+
+      setCallDetails(call);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
