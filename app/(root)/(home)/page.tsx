aec3e7fc1e01e36aec466a1f1a1bbe892fb3f3fd
Keith Primas <keithprimas@gmail.com>
2024-05-12 20:05:29 -0400

    fixed homecard date

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 const Home = () => {
   const now = new Date();
-  const time = now.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
   const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
 
   return (
@@ -11,8 +10,7 @@ const Home = () => {
         <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
             <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
                 <div className='flex flex-col gap-2'>
-                  <h1 className='text-4xl font-extrabold lg:text-7xl'>{time}</h1>
-                  <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
+                  <p className='text-4xl font-bold text-sky-1 lg:text-2xl'>{date}</p>
                 </div>
             </div>
         </div>
