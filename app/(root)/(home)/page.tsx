fdad2692cf80753731f65c4bd9b7aaff212dc441
Keith Primas <keithprimas@gmail.com>
2024-05-13 06:12:22 +0500

    time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,8 +13,8 @@ const Home = () => {
         <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
             <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
                 <div className='flex flex-col gap-2'>
-                  <h1 className='text-4xl font-extrabold lg:text-7xl'>Let's get started!</h1>
-                  <p className='text-lg font-medium text-sky-1 lg:text-2xl'></p>
+                  <h1 className='text-4xl font-extrabold lg:text-7xl'>{time}</h1>
+                  <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
                 </div>
             </div>
         </div>
